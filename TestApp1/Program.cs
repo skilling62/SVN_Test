01c7c39c1519d307e3ccac67d6c0f35b5e7844d0
skilling62 <sam.skilling@ntlworld.com>
2016-09-30 13:01:14 +0100

    C# sharp introduction files

---
+++ TestApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//This is a namespace - a way of grouping things together
+namespace TestApp1
+{
+    //This is a class - contains functions and variables, things that we want to run
+    class Program
+    {
+        //This is a funciton
+        static void Main(string[] args)
+        {
+            //This is where the program starts. Console is a class (contains functionality)
+            Console.WriteLine("Type a number, any number");
+            ConsoleKeyInfo keyinfo = Console.ReadKey();
+            Console.WriteLine("Did you type {o}", keyinfo.KeyChar.ToString());
+        }
+    }
+}
