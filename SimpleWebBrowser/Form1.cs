1d0ab4f29ed95fd514d43283c5dc4d2ad1fe01fc
skilling62 <sam.skilling@ntlworld.com>
2016-10-14 08:45:14 +0100

    Tutorial 5, initial files for a simple web browser - windows form application

---
+++ SimpleWebBrowser/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SimpleWebBrowser
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// When the button is clicked, this will be executed
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button1_Click(object sender, EventArgs e)
+        {
+            richTextBox1.AppendText("A button was clicked\n");
+        }
+    }
+}
