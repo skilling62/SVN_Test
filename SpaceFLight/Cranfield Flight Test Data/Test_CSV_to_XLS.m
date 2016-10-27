0c33485b4610cf81235d2b42d66ab0c239dc3414
skilling62 <sam.skilling@ntlworld.com>
2016-10-27 10:09:17 +0100

    JJ Remove flight test folder

--- SpaceFLight/Cranfield Flight Test Data/Test_CSV_to_XLS.m
+++
@@ -1,9 +0,0 @@
-yourfolder='\\nstu-uwe02.campus.ads.uwe.ac.uk\users2$\j69-lewis\Personal\MATLAB\Year 3\Cranfield Flight Test Data'
-d=dir([yourfolder '\*.csv']);
-files={d.name};
-for k=1:numel(files)
-   old_name=files{k}; 
-   [~,~,b] = xlsread(old_name) ;
-   new_name=strrep(old_name,'csv','xls')
-   xlswrite(new_name,b);
-end
--- SpaceFLight/Cranfield Flight Test Data/tset1.m
+++
@@ -1,4 +0,0 @@
-t = 2.5;
-W_d = t/(2*pi);
-zeta = 1.1;
-W_n = W_d*sqrt(zeta^2 - 1)
