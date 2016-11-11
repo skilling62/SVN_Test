ff5687a612238b3d8cf2bb4fe8cf0c0a084331cc
skilling62 <sam.skilling@ntlworld.com>
2016-11-11 11:24:25 +0000

    Edited the Aircraft_Data.m file (JL) Added wing profile designs from java foil (JL)

--- SpaceFLight/Aircraft_Data.m
+++ SpaceFLight/Aircraft_Data.m
@@ -11,100 +11,12 @@
 function Aircraft_Data ()
 
 global U_0 m S_w S_v b_w b_v I_z I_x I_y Mach AR_w AR_v l_v V_v 
-global EffFac_W CL_AlphaV dSigmaBYdBeta C_nBeta_wt EffFac_V
+global EffFac_W CL_Av dEpsBYdAlpha C_nBeta_wt EffFac_V
 
-   		
-    kts2ms = 0.51444; % kts to m/s 1 = 0.51444
-	
-    addpath .\Cranfield_Flight_Test_Data;
-    
-    data1 = xlsread('Phugoid_GpA.xls');
-	data2 = xlsread('Phugoid_GpB.xls');
-	data3 = xlsread('Phugoid_GpC.xls');
+    % Name of the MAT-file that will be generated
+    cfgmatfile = 'JetStream';
 
-    U1_0 = data1(1,4);
-	U2_0 = data2(1,4);
-	U3_0 = data3(1,4);
-
-    U_0 = (((U1_0 + U2_0 + U3_0) / 3) * kts2ms),	% m/s, U_0 is the average speed of the phugoid
-    
-    Kts2Mach = 0.0015; % Knots to Mach Number convertion 1 = 0.0015
-    
-    Mach = U_0 * Kts2Mach;
-    
-   %     Ref: http://www.airlines-inform.com/commercial-aircraft/Jetstream-31.html
-
-	% Wing Specifications
-    S_w = 25.083; 	% m^2, Main Wing Area
-	b_w = 15.85;	% m, Main Wing Span
-    d_w = 14.351;   % m, Main Wing Length
-    e_w = (b_w + d_w) / 2; % m
-	AR_w = (b_w^2) / S_w; % Aspect Ratio for Main Wing
-    Cbar = 1.71704;  % m, Mean Aerodynamic Chord
-   
-    
-    
-	Y1 = (b_w / 2) - ((b_w / 2) * 0.3); % Spanwise Distance from Centreline to the inboard edge of the aileron control percentage taken estimate 
-	EffFac_W = Y1 / (b_w / 2); % Efficiency factor of the main wing
-    K = -0.12;      % Empirical factor for Cn_lilDelta_Aileron Nelson, R -  CH 3 pg 122 fig 3.12
-    
-    
-    
-    % Vertical Stabiliser Specification 
-    S_v = 5.639215; % m^2, Vertical Stabiliser area 
-	b_v = 3.0861;   % m, Height of Verticial Stabiliser
-    AR_v = (b_v^2) / S_v; %  Aspect Ratio for Verticial Stabiliser
-    l_v = 0;		% Distance from CG to vertical Stabiliser MAC
-	V_v = (l_v * S_v) / (S_w * Cbar);		% Vertical Stabiliser volume ratio
-    Y2 = (b_v / 2) - ((b_v / 2) * 0.95); % Spanwise Distance from Centreline to the inboard edge of the rudder percentage taken estimate 
-    EffFac_V = Y2 / (b_v / 2); % Efficiency factor of the Veritical Stabiliser
-    CM_alphT = -(EffFac_V) * V_v * CL_AlphaV * (1 - dSigmaBYdBeta); 
-    
-    
-    % Horizontal Stabilisers Specification
-    S_h = 7.80386;  % m^2, Horizontal Stabiliser area 
-    b_h = 6.64464;  % m, Span of Horizontal Stabiliser 
-    AW_h = (b_h^2) / S_h   %  Aspect Ratio for Horizontial Stabiliser
-    
-    
-    
-    % Weights of Aircraft
-    m = 6348; 		% kg, Weight of the Aircraft
-	MTOW = 7350;	% kg, Max Take-off Weight of the Aircraft
-	MLW = 7080;		% kg, Max Landing Weight of the Aircraft
-	OEW = 4720;		% kg, Operating Empty Weight of the Aircraft
-	MZFW = 6760;	% kg, Max Zero Fuel Weight of the Aircraft
-	
-    % Coefficients of Aircraft		
-    
-    % Main Wing
-    CL_0w = 0.368;
-    CD_0w = 0.01852;
-    CM_Ow = -0.059;      % 25% MAC
-	CL_Aw = 0.0788;
-    CD_Aw = 0.008956;
-    CM_Aw = -0.0014;
-      
-    % Horizontal Stabilisers
-    CL_0h = 0.00;
-    CD_0h = 0.01217;
-    CM_0h = -0.00;        % 25% MAC
-    
-    % Fuselage data
-    K_n = 1;    % Empirical wing-body interference factor tha is a function of the fuselage geometry
-    K_RL = 1;   % Empirical correction factor that is a function of the fuselage Reynolds Number
-    S_fs = 1;   % The projected side area of the fuselage
-    L_f = 1;    % Length of the fuselage
-    C_nBeta_wt = -K_n * K_RL * ((S_fs * L_f)/(S_w * b_w));
-    
-    
-    CL_AlphaV = 1;
-    dSigmaBYdBeta = 1;
-    
-    
-        
-	
-    %% CG Calculations
+   	    %% CG Calculations
     
     M_W = 6348; 
     M_A = 5.643; 
@@ -137,9 +49,136 @@ global EffFac_W CL_AlphaV dSigmaBYdBeta C_nBeta_wt EffFac_V
     SUM_Mass = sum([AC_W M_W StRow3_W StRow4_W StRow5_W StRow6_W CO_W]);
     SUM_Mom = sum([AC_Mo M_M StRow3_Mo StRow4_Mo StRow5_Mo StRow6_Mo CO_Mo]);
 
-    CG = (((SUM_Mom / SUM_Mass) - 5.149)) * (100/1.717)
+    CG = (((SUM_Mom / SUM_Mass) - 5.149)) * (100/1.717);
+    
+    %% U_0 Calculations
+    
+    kts2ms = 0.51444; % kts to m/s 1 = 0.51444
 	
-    %% Calulating Moments of Inertia
+    addpath .\Cranfield_Flight_Test_Data;
+    
+    data1 = xlsread('Phugoid_GpA.xls');
+	data2 = xlsread('Phugoid_GpB.xls');
+	data3 = xlsread('Phugoid_GpC.xls');
+
+    U1_0 = data1(1,4);
+	U2_0 = data2(1,4);
+	U3_0 = data3(1,4);
+
+    U_0 = (((U1_0 + U2_0 + U3_0) / 3) * kts2ms);	% m/s, U_0 is the average speed of the phugoid
+    
+    Kts2Mach = 0.0015; % Knots to Mach Number convertion 1 = 0.0015
+    
+    Mach = U_0 * Kts2Mach;
+    
+    %   Ref: http://www.airlines-inform.com/commercial-aircraft/Jetstream-31.html
+    %% Aircraft Specifications
+    
+    % Weights of Aircraft
+    m = 6348; 		% kg, Weight of the Aircraft
+	MTOW = 7350;	% kg, Max Take-off Weight of the Aircraft
+	MLW = 7080;		% kg, Max Landing Weight of the Aircraft
+	OEW = 4720;		% kg, Operating Empty Weight of the Aircraft
+	MZFW = 6760;	% kg, Max Zero Fuel Weight of the Aircraft
+    
+    %% Coefficients of Aircraft		
+    % Main Wing
+    
+    data4 = xlsread('JavaFoil_MainWingData');
+    CL_w = data4(:,2);
+    CD_w = data4(:,3);
+    Alpha = data4(:,1);
+    
+    figure
+    subplot(2,1,1)
+    plot(Alpha, CL_w, 'b')
+    subplot(2,1,2)
+    plot(Alpha, CD_w, 'k')
+    
+    CL_0w = 0.368;
+    CD_0w = 0.01852;
+    CM_Ow = -0.059;      % 25% MAC
+	
+    CL_Aw = 0.0788;
+    CD_Aw = 0.008956;
+    CM_Aw = -0.0014;
+      
+    % Horizontal Stabilisers
+    CL_0h = 0.00;
+    CD_0h = 0.01217;
+    CM_0h = -0.00;        % 25% MAC
+    
+    CL_Ah = 1;
+    CD_Ah = 1;
+    CM_Ah = 1;
+    
+    % Vertical Stabilisers
+    data5 = xlsread('JavaFoil_TailData');
+    CL_v = data5(:,2);
+    CD_v = data5(:,3);
+   
+    figure
+    subplot(2,1,1)
+    plot(Alpha, CL_v, 'b')
+    subplot(2,1,2)
+    plot(Alpha, CD_v, 'k')
+    
+    CL_0v = 1;
+    CD_0v = 1;
+    CM_0v = 1;
+    
+    CL_Av = 1;
+    CD_Av = 1;
+    CM_Av = 1;
+    
+        
+    %% Fuselage data
+    K_n = 1;    % Empirical wing-body interference factor tha is a function of the fuselage geometry
+    K_RL = 1;   % Empirical correction factor that is a function of the fuselage Reynolds Number
+    S_fs = 1;   % The projected side area of the fuselage
+    L_f = 1;    % Length of the fuselage
+    
+    
+	%% Wing Specifications
+    % Main Wing
+    S_w = 25.083; 	% m^2, Main Wing Area
+	b_w = 15.85;	% m, Main Wing Span
+    d_w = 14.351;   % m, Main Wing Length
+    e_w = (b_w + d_w) / 2; % m
+	AR_w = (b_w^2) / S_w; % Aspect Ratio for Main Wing
+    Cbar = 1.71704;  % m, Mean Aerodynamic Chord
+        
+	Y1 = (b_w / 2) - ((b_w / 2) * 0.3); % Spanwise Distance from Centreline to the inboard edge of the aileron control percentage taken estimate 
+	EffFac_W = Y1 / (b_w / 2); % Efficiency factor of the main wing
+    K = -0.12;      % Empirical factor for Cn_lilDelta_Aileron Nelson, R -  CH 3 pg 122 fig 3.12
+    
+       
+    % Vertical Stabiliser  
+    S_v = 5.639215; % m^2, Vertical Stabiliser area 
+	b_v = 3.0861;   % m, Height of Verticial Stabiliser
+    AR_v = (b_v^2) / S_v; %  Aspect Ratio for Verticial Stabiliser
+    l_v = 0;		% Distance from CG to vertical Stabiliser MAC
+	V_v = (l_v * S_v) / (S_w * Cbar);		% Vertical Stabiliser volume ratio
+    Y2 = (b_v / 2) - ((b_v / 2) * 0.95); % Spanwise Distance from Centreline to the inboard edge of the rudder percentage taken estimate 
+    EffFac_V = Y2 / (b_v / 2); % Efficiency factor of the Veritical Stabiliser
+    CM_Av = -(EffFac_V) * V_v * CL_Av * (1 - dEpsBYdAlpha); 
+    
+    
+    % Horizontal Stabilisers Specification
+    S_h = 7.80386;  % m^2, Horizontal Stabiliser area 
+    b_h = 6.64464;  % m, Span of Horizontal Stabiliser 
+    AW_h = (b_h^2) / S_h;   %  Aspect Ratio for Horizontial Stabiliser
+      
+    %% Other Parametres
+    
+    C_nBeta_wt = -(K_n) * K_RL * ((S_fs * L_f)/(S_w * b_w));
+    
+    dEpsBYdAlpha = (2 * CL_Aw) / (pi * AR_w);
+    dSigmaBYdBeta = 1;  
+    
+    
+		
+    %%  Moments of Inertia Calulations
     
     % Medium Twin (Cessna 402) pg. 34 - FLIGHT DYNAMICS FOR MICROSOFT FLIGHT SIMULATOR
     R_x = 0.373;
@@ -148,10 +187,23 @@ global EffFac_W CL_AlphaV dSigmaBYdBeta C_nBeta_wt EffFac_V
     
     g = 32.2;
     
-    I_x = ((MTOW / g) * R_x * b_w/2) % Moment of inertia on the x axis
-    I_y = ((MTOW / g) * R_y * d_w/2) % Moment of inertia on the x axis
-    I_z = ((MTOW / g) * R_z * e_w/2) % Moment of inertia on the x axis
+    I_x = ((MTOW / g) * R_x * b_w/2); % Moment of inertia on the x axis
+    I_y = ((MTOW / g) * R_y * d_w/2); % Moment of inertia on the x axis
+    I_z = ((MTOW / g) * R_z * e_w/2); % Moment of inertia on the x axis
     
     
+    %% OTHER SIMULATION PARAMETERS %%%
     
+    % WMM-2000 date [day month year]
+    dmy = [13 05 2002];
+ 
+%     d = datetime('today')
+    
+    % Save workspace variables to MAT file
+    save(cfgmatfile);
+
+    % Output a message to the screen
+    fprintf(strcat('\n Aircraft configuration saved as:\t', strcat(cfgmatfile),'.mat'));
+    fprintf('\n');
+
 	end
--- SpaceFLight/DutchRoll_Func.m
+++ SpaceFLight/DutchRoll_Func.m
@@ -1,26 +1,25 @@
 function DutchRoll_Func ()
 
 global S_v S_w b_w b_v V_v l_v m U_0 I_z Rho
-global CL_AlphaV dSigmaBYdBeta C_nBeta_wt EffFac_V
+global CL_Av dEpsBYdAlpha C_nBeta_wt EffFac_V
 
 addpath .\Cranfield_Flight_Test_Data;
 
 %% Data Needed for Calculations
 
-C_yBetaTail = CL_AlphaV * EffFac_V * (S_v / S_w);
+C_yBetaTail = CL_Av * EffFac_V * (S_v / S_w);
 
 C_yr = -2 * (-C_yBetaTail) * (l_v / b_v);
 
-C_nr = -2 * EffFac_V * V_v * (l_v / b_v) * CL_AlphaV;
+C_nr = -2 * EffFac_V * V_v * (l_v / b_v) * CL_Av;
 
-C_yBeta = -EffFac_V * (S_v / S_w) * CL_AlphaV * (1 + dSigmaBYdBeta);
+C_yBeta = -EffFac_V * (S_v / S_w) * CL_Av * (1 + dEpsBYdAlpha);
 
-C_nBeta = C_nBeta_wt + (EffFac_V * V_v * CL_AlphaV * (1 + dSigmaBYdBeta));
+C_nBeta = C_nBeta_wt + (EffFac_V * V_v * CL_Av * (1 + dEpsBYdAlpha));
 
 %% Calculations
 
 Pg_data = xlsread('Phugoid_GpA.xls');
- 
 Dens_Calc(358,Pg_data(1,5),18,1012)
 
 Q = 0.5 * Rho * (U_0)^2;
