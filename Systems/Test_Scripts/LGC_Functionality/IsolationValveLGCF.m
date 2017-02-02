bd52a4f31034519f1f0b9e432967aeceda5705f5
skilling62 <sam.skilling@ntlworld.com>
2017-02-02 10:54:33 +0000

    (SS) Moved the Isolation Valve test script into separate folders

---
+++ Systems/Test_Scripts/LGC_Functionality/IsolationValveLGCF.m
@@ -0,0 +1,11 @@
+ iv_open = On;
+
+% if(time <60) 
+%         iv_open = On;        
+%     elseif (time <120)
+%         iv_open = On;
+%     elseif (time <180)
+%         iv_open = Off;
+%     elseif (time <240)
+%             iv_open = Off;
+%     end
---
+++ Systems/Test_Scripts/Tester_Functionality/IsolationValveTesterF.m
@@ -0,0 +1,18 @@
+%%tester functionality%%
+if (time<60)
+    iv_err = NoError;
+else
+    iv_err = Error;
+end
+
+%   if (time < 60)
+%         iv_err  = Error; 
+%     elseif (time <120)
+%          iv_err  = NoError;
+%     elseif (time <180)
+%         iv_err = NoError;
+%     elseif (time <240)
+%         iv_err= Error;
+%   end
+    
+  
