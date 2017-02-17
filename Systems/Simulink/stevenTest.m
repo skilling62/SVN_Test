a96179248735148847a973b8ccd3902b0f69ad87
skilling62 <sam.skilling@ntlworld.com>
2017-02-17 12:36:30 +0000

    (SS) Modified Isolation Valve Test Script

--- Systems/Simulink/stevenTest.m
+++ Systems/Simulink/stevenTest.m
@@ -4,7 +4,38 @@
 A = evalin ('base','HydrOut.time');
 B = evalin ('base', 'HydrOut.signals.values');
 %% Test 1: No Supply
-assert(A(1) == 0, 'time at t=0 is not 0')
 
-%% Test 2: Value at t=0
-assert(B(1) == 5,'invalid value for HydrOut')
+% Define time range (15)
+range = A(A<15);
+midpoint = range(length(range)/2);
+index = find(A == midpoint);
+
+% Test output
+assert(B(index) == 0);
+
+%% Test 2: Uncommanded On
+% Define time range (15)
+range = A(A>=15 & A<30);
+midpoint = range(length(range)/2);
+index = find(A == midpoint);
+
+% Test output
+assert(B(index) == 0,'Valve not passing supply');
+
+%% Test 3: Normal Operation On
+% Define time range (15)
+range = A(A>=30 & A<45);
+midpoint = range(length(range)/2);
+index = find(A == midpoint);
+
+% Test output
+assert(B(index) == 1);
+
+%% Test 4: Uncommaned Off
+% Define time range (15)
+range = A(A>=45 & A<60);
+midpoint = range(length(range)/2);
+index = find(A == midpoint);
+
+% Test output
+assert(B(index) == 1);
