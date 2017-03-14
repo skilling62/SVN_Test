226a462463ceeccfad43e92a2c4572c9aab8d0d4
skilling62 <sam.skilling@ntlworld.com>
2017-03-14 10:09:18 +0000

    (SS) Modify the selector valve post processing script with new logic (Uncommanded down on test 4 rather than neutral)

--- Systems/Simulink/Model_Post_Processing_Scripts/Results_Selector_Valve.m
+++ Systems/Simulink/Model_Post_Processing_Scripts/Results_Selector_Valve.m
@@ -1,6 +1,7 @@
 % This function evaluates the simulation results acheived from running the 
 % following test script: 
 % SelectorValve (LGCF and Tester)
+% We keep the hydraulics supply on for test duration
 
 % preconditions (shared variables)
 A = evalin ('base','HydrOutDown.time');
@@ -8,7 +9,7 @@ B = evalin ('base', 'HydrOutDown.signals.values');
 C = evalin ('base','HydrOutUp.time');
 D = evalin ('base','HydrOutUp.signals.values');
 
-%% Test 1: Selector Position = Neutral, Hydraulic Supply = On, (Regular Operation Off)
+%% Test 1: Selector Position = Neutral, no error (Regular Operation Off)
 
 % Define time range (15)
 range = A(A<15);
@@ -19,48 +20,48 @@ index = find(A == midpoint);
 assert(B(index) == 0);
 assert(D(index) == 0);
 
-%% Test 2: Selector Position = Neutral(+Error), Hydraulic Supply = On, (Uncommanded Down)
+%% Test 2: Selector Position = Neutral, error (Uncommanded Down)
 range = A(A>=15 & A<30);
 midpoint = range(length(range)/2);
 index = find(A == midpoint);
 assert(B(index) == 1);
 assert(D(index) == 0);
 
-%% Test 3: Selector Position = Up, Hydraulic Supply = On, (Regular Operation Retraction)
+%% Test 3: Selector Position = Up, no error (Regular Operation Retraction)
 range = A(A>=30 & A<45);
 midpoint = range(length(range)/2);
 index = find(A == midpoint);
 assert(B(index) == 0);
 assert(D(index) == 1);
 
-%% Test 4: Selector Position = Up(+Error), Hydraulic Supply = On, (Extension Error)
+%% Test 4: Selector Position = Up, error (Uncommanded Down)
 range = A(A>=45 & A<60);
 midpoint = range(length(range)/2);
 index = find(A == midpoint);
-assert(B(index) == 0);
+assert(B(index) == 1);
 assert(D(index) == 0);
 
-%% Test 5: Selector Position = Down, Hydraulic Supply = On, (Regular Operation Extension)
+%% Test 5: Selector Position = Down, no error (Regular Operation Extension)
 range = A(A>=60 &A<75);
 midpoint = range(length(range)/2);
 index = find(A == midpoint);
 assert(B(index) == 1);
 assert(D(index) == 0);
 
-%% Test 6: Selector Position = Down(+Error), Hydraulic Supply = On, (Fails to move to the down position)
+%% Test 6: Selector Position = Down, error (Fails to move to the down position)
 range = A(A>=75 & A<90);
 midpoint = range(length(range)/2);
 index = find(A == midpoint);
 assert(B(index) == 0);
 assert(D(index) == 0);
-%% Test 7: Selector Position = Down and Up, Hydraulic Supply = On, (Conflicting LGC Commands)
+%% Test 7: Selector Position = Down and Up, no error (Conflicting LGC Commands)
 range = A(A>=90 & A<105);
 midpoint = range(length(range)/2);
 index = find(A == midpoint);
 assert(B(index) == 0);
 assert(D(index) == 0);
 
-%% Test 8: Selector Position = Down and Up (+Error), Hydraulic Supply = On, (Conflicting LGC Commands)
+%% Test 8: Selector  Position = Down and Up, error (Conflicting LGC Commands)
 range = A(A>=105 & A<120);
 midpoint = range(length(range)/2);
 index = find(A == midpoint);
