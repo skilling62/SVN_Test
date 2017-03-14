a930866cfd632bf44a81c949debdef13aee105b8
skilling62 <sam.skilling@ntlworld.com>
2017-03-14 08:45:58 +0000

    (SS) Add Timestamp to ProcessResults.m

--- Systems/Simulink/ProcessResults.m
+++ Systems/Simulink/ProcessResults.m
@@ -2,18 +2,23 @@
 % A runtests function is called, which outputs the results of the user
 % specified test as a .csv 
 
+StampTime = datestr(now,'_HH:MM:SS dd/mm/yy');
 addpath Model_Post_Processing_Scripts
 
-% List of tests (to be assigned to the test name variable
-% Model Verification Tests
-    % Results_IsolationValve_Emergency_Valve
-    % Results_Shuttle_Valves
-    % Results_Uplocks
-    % Results_Shuttle_Valves
-
+% List of available tests
+%% Model Verification
+    Results_IsolationValve_Emergency_Valve
+    Results_Selector_Valves
+    Results_Uplocks
+    Results_Shuttle_Valves
+    
+%% System Verification
+    
+%% Run test and export results to .csv 
 % testname = 'Results_Shuttle_Valves';
 % result = runtests(testname);
 % rt = table(result);
 % cd('../Test_Results')
+% testname(length(testname)+1:length(testname)+18) = StampTime;
 % testname(length(testname)+1:length(testname)+4) = '.csv';
 % writetable(rt,testname);
