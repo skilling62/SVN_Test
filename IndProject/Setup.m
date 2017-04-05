94f9b7fb2f7a1786ac0305dcce005fb1bdc4934c
skilling62 <sam.skilling@ntlworld.com>
2017-04-05 21:05:06 +0100

    delete pose estimate function (to be replaced with script)

--- IndProject/Setup.m
+++ IndProject/Setup.m
@@ -1,9 +1,8 @@
 % -------------------------------------------------------------------------
 % This script imports the navdata and video feeds for processing in:
 % Navdata - poseEstimate.m and CombinedVO.m
-% Video Feed
-
-
+% Video Feed - VO.m and combined VO.m
+% -------------------------------------------------------------------------
 close all
 clearvars;
 clc
@@ -32,17 +31,17 @@ M = M(index:size(M,1),:);
 load ('cameraParams.mat')
 cameraStruct = toStruct(cameraParams);
 
-% Downward Facing Camera
+% Downward Facing Camera Parameters
 % cameraStruct.IntrinsicMatrix = [686.994766, 0, 329.323208; 0, 688.195055, 159.323007; 0, 0, 1]';
 
-% Forward Facing Camera
+% Forward Facing Camera Parameters
 cameraStruct.IntrinsicMatrix = [561.999146, 0, 307.433982; 0, 561.782697, 190.144373; 0, 0, 1]';
 cameraParams = cameraParameters(cameraStruct);
 
 %% ------------------------------------------------------------------------
 %  Read Video File
 %  ------------------------------------------------------------------------
-% Read from video file only once established in the hover
+% Read from video file from a certain point if required
 k = find(M(:,11)>=0.68,1);
 t0Vid = (M(k,1))/1000;
 t0Vid = 0.0;
@@ -59,19 +58,15 @@ vidHeight = vidObj.Height;
 vidDuration = vidObj.Duration;
 vidFrameRate = vidObj.FrameRate;
 
-% Create a MATLAB® movie structure array, s
-% s = struct(field,value). If value is a cell array, s is a structure
-% array with the same dimensions as value. Fields: cdata, colormap,
-% timeStamp
+% Create a movie structure array to store video parameters in
 s = struct('cdata',zeros(vidHeight,vidWidth,3,'uint8'),'timeStamp',[],...
     'colormap',[]);
 
-% hasFrame returns a logical 1(true) if there is a video frame available to
-% read from the file
 k = 1;
 while hasFrame(vidObj)
         frame = rgb2gray(readFrame(vidObj));
-        %If the frame is blacked out skip to the next frame
+        % If the frame is blacked out skip to the next frame (threshold =
+        % intensity of 10)
         if (sum(frame(vidHeight/2,:))/(vidWidth)) > 10
             s(k).cdata = undistortImage(frame, cameraParams);
             s(k).timestamp = vidObj.CurrentTime;
@@ -90,7 +85,8 @@ sMatrix = zeros(vidHeight,vidWidth,length(s));
         end
     end
 
-% Remove blacked out frames from the array (a black frame has all elements = 0)    
+% Remove blacked out frames from the array (a black frame in the array has 
+% all elements = 0)    
 remFrame = sum(sMatrix(vidHeight/2,:,1:length(s)));
 keepFrame = remFrame~=0;
 s = s(keepFrame);
